37c5ac275a71b49f21a2dd423b88e7274e9395e7
Matthew Wilson <msw09g@gmail.com>
2025-11-11 22:37:01 -0500

    [refactor] Make display_out a wire rather than a reg

--- rtl/seven_seg_driver_decimal.v
+++ rtl/seven_seg_driver_decimal.v
@@ -1,22 +1,24 @@
 module seven_seg_driver_decimal(
     input [3:0] number,
-    output reg [6:0] display_out
+    output [6:0] display_out
 );
-
+reg [6:0] segment_val;
 always @(*) begin
-    display_out = {7{1'b1}};
+    segment_val = {7{1'b1}};
     case(number)
-        4'h0 : display_out = 7'b0000001;
-        4'h1 : display_out = 7'b1001111;
-        4'h2 : display_out = 7'b0010010;
-        4'h3 : display_out = 7'b0000110;
-        4'h4 : display_out = 7'b1001100;
-        4'h5 : display_out = 7'b0100100;
-        4'h6 : display_out = 7'b0100000;
-        4'h7 : display_out = 7'b0001111;
-        4'h8 : display_out = 7'b0000000;
-        4'h9 : display_out = 7'b0001100;
+        4'h0 : segment_val = 7'b0000001;
+        4'h1 : segment_val = 7'b1001111;
+        4'h2 : segment_val = 7'b0010010;
+        4'h3 : segment_val = 7'b0000110;
+        4'h4 : segment_val = 7'b1001100;
+        4'h5 : segment_val = 7'b0100100;
+        4'h6 : segment_val = 7'b0100000;
+        4'h7 : segment_val = 7'b0001111;
+        4'h8 : segment_val = 7'b0000000;
+        4'h9 : segment_val = 7'b0001100;
     endcase
 end
+
+assign display_out = segment_val;
 
 endmodule
